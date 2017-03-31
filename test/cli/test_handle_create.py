6f33347a102cd2bcd79173061c10378bc80cd814
Matthijs Bos <matthijs_vlaarbos@hotmail.com>
2017-03-31 22:48:02 +0200

    spec'ed mock

--- test/cli/test_handle_create.py
+++ test/cli/test_handle_create.py
@@ -2,13 +2,14 @@ import unittest
 import unittest.mock as mock
 
 import vagrepo.cli
+import vagrepo.repository
 
 class HandleCreateTestCase(unittest.TestCase):
 
     def test_handle_create(self):
         '''test vagrepo.cli.handle_create'''
         mock_namespace = mock.Mock()
-        mock_repository = mock.Mock()
+        mock_repository = mock.Mock(spec=vagrepo.repository.Repository)
 
         vagrepo.cli.handle_create(mock_namespace, mock_repository)
         mock_repository.create.assert_called_with(mock_namespace.name, mock_namespace.description)
